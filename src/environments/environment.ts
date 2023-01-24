55360d0a5ebf41a910e53707431535b48bd730dd
Paulina Esperanza Martínez Muñiz <pesperanza@live.com>
2023-01-23 21:40:43 -0600

    API Implementation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'f672fed363mshb873d20fcba4120p1ef6adjsn56762c6c49f4'
+
+
 };
 
 /*
